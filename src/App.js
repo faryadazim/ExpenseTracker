b80db7646a46f369b5e1284ef8676112165877a3
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2021-09-08 20:37:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import { useState } from "react";
 import './App.css';
 
 function App() {
-   
+
   const [state, setstate] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
-       <button onClick={()=>{setstate(state-1)}}>Decrease</button>
-       <br />
-       <span>
-         {state}
-       </span>
-       <br />
-       <button onClick={()=>{setstate(state+1)}}>Increase</button>
+        <button onClick={() => {
+          if (state === 0) {  console.log("Decrease");  } else { setstate(state - 1)  } }}>Decrease</button>
+        <br />
+        <span>
+          {state}
+        </span>
+        <br />
+        <button onClick={() => { setstate(state + 1) }}>Increase</button>
       </header>
     </div>
   );
