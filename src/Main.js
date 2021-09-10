fb6a2d1af44ef7629fa4fe9167cf7a29d5a247d6
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2021-09-10 12:13:10 -0700

    designed

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
 const Main = () => {
+    let transactions = [
+        { amount: 500, desc: "Salary" }, { amount: -30, desc: "Camera" }
+        , { amount: -20, desc: "Book" }
+    ]
     return (
         <>
             <div className="container main-container">
                 <div className="header">
-                <h2>Expense Tracker</h2>    
+                    <h2>Expense Tracker</h2>
                 </div>
                 <div className="banner">
                     <div className="current-balance">
-                        <h4> Your Balance is : $260</h4> 
-                         </div>
+                        <h4>   BALANCE  : $260</h4>
+                    </div>
                     <div className="breif-balance">
                         <div className=" text-start ps-2 income-balance">Income :   $567</div>
                         <div className=" text-end pe-2  expense-balance">Expense :   $267</div>
@@ -18,18 +22,16 @@ const Main = () => {
                     </div>
                 </div>
                 <div className="history-section">
-                <h4>History</h4>
+                    <h4>History</h4>
                     <ul className="scrol-here">
-                       
-                        <li> <button className="btn  cross">x</button> <span className="history-list">  <span>Iphone</span><span>-99$</span> </span> </li>
-                        <li> <button className="btn  cross">x</button> <span className="history-list">  <span>Iphone</span><span>-99$</span> </span> </li>
-                        <li> <button className="btn  cross">x</button> <span className="history-list">  <span>Iphone</span><span>-99$</span> </span> </li>
-                        <li> <button className="btn  cross">x</button> <span className="history-list">  <span>Iphone</span><span>-99$</span> </span> </li>
-                        <li> <button className="btn  cross">x</button> <span className="history-list">  <span>Iphone</span><span>-99$</span> </span> </li>
-                        <li> <button className="btn  cross">x</button> <span className="history-list">  <span>Iphone</span><span>-99$</span> </span> </li>
-                        <li> <button className="btn  cross">x</button> <span className="history-list">  <span>Iphone</span><span>-99$</span> </span> </li>
-                         
-                       
+                        {
+                            transactions.map((items) => {
+                              return  <li> <button className="btn  cross">x</button> <span className="history-list">  <span>{items.desc}</span><span>{items.amount}$</span> </span> </li>
+                            })
+                        }
+
+
+
                     </ul>
                 </div>
                 <div className="form-section">
@@ -39,21 +41,20 @@ const Main = () => {
                         <label htmlFor="disc">
                             Enter Discription
                         </label>
-                            <input id="disc" className="form-control w-100" type="text" placeholder="Label Discription" required />
-                    
+                        <input id="disc" className=" form-control-sm form-control w-100" type="text" placeholder="Label Discription" required />
+
                         <label htmlFor="amount">
                             Enter Amount
                         </label>
-                            <input  id="amount" className="form-control w-100" type="text" placeholder="Positive => +Income  /Negative -Expense"  required/>
-                      
-                    <button className="btn mt-2  btn-sm btn-outline-secondary w-100  " type="submit">
-                        Add Transaction
-                    </button>
+                        <input id="amount" className=" form-control-sm form-control w-100" type="text" placeholder="Positive => +Income  /Negative -Expense" required />
 
-                    </form>  
+                        <input className="btn mt-2  btn-sm btn-outline-secondary w-100  " type="submit" value="Add Transaction" />
+
+
+                    </form>
                 </div>
             </div>
-           
+
         </>
     )
 }
