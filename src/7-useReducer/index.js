35293cd2eefdccf881fd4ed33fe9e9a4db8868c4
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2021-09-15 07:14:44 -0700

    ueReducer

--- src/7-useReducer/index.js
+++ src/7-useReducer/index.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import Model from "./modal";
+import { Data } from "../Data";
 
+// UseReducer
 const Index = () => {
-    return (
-        <>
-          <h3> b </h3>  
-        </>
-    )
-}
+  const [persons, setPersons] = useState(Data);
+  const [showModel, setShowModel] = useState(false);
+  const [name, setName] = useState(""); 
+  const submitHandle = (e) => {
+    e.preventDefault();
+    if (name) {
+      setShowModel(true);
+      let CreatedObj = {
+      id: new Date().getTime().toString(),
+      name 
+    };
+    setPersons([...persons, CreatedObj]);
+    setName('');
+    } else {
+      setShowModel(true); 
+    }
+   
 
-export default Index
+  };
+
+  return (
+    <>
+      {showModel && <Model />}
+      <form onSubmit={submitHandle}>
+        <div>
+          <input
+            value={name}
+            type="text"
+            name="firstName"
+            placeholder="Name"
+            className="form-control my-2 w-50"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit" className="btn btn-info btn=sm w-50">
+          Add Person
+        </button>
+      </form>
+      <ul>
+
+        {
+          persons.map((e) => {
+            
+            return <li>{ e.name}</li>
+            
+           
+          })
+        }
+      </ul>
+    </>
+  );
+};
+
+export default Index;
---
+++ src/7-useReducer/modal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const modal = () => {
+    return (
+        <>
+            i'm Model
+        </>
+    )
+}
+
+export default modal
